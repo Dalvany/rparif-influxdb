8fc11036344758082a56d2e06b69515b2a95bd18
Dalvany <Dalvany@users.noreply.github.com>
2020-05-26 16:07:48 +0200

    Release using travis

---
+++ ci/before_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+cargo install cargo-deb
+
+# Install the Rust stdlib for the current target
+rustup target add $TARGET
+
+# Download the Raspberry Pi cross-compilation toolchain if needed
+if [ "$TARGET" = "arm-unknown-linux-gnueabihf" ]; then
+  git clone --depth=1 https://github.com/raspberrypi/tools.git /tmp/tools
+  export PATH=/tmp/tools/arm-bcm2708/arm-linux-gnueabihf/bin:$PATH
+fi
+
+# Compile the binary and make a debian package
+cargo-deb --target=$TARGET
